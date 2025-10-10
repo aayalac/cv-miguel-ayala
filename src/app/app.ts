4c2b22b37cfd9c1f7018bab80fe099f688ae8e07
Miguel Angel Ayala Castillo <ingeniero.ayala@outlook.com>
2025-10-10 17:15:01 -0500

    feat: actualización presentacion y estilos

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { Estudios } from './estudios/estudios';
 import { Presentacion } from './presentacion/presentacion';
 import { Experiencia } from './experiencia/experiencia';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Estudios, Presentacion, Experiencia],
+  imports: [RouterOutlet, Estudios, Presentacion, Experiencia, MatToolbarModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css'  
 })
 export class App {
   protected readonly title = signal('cv-miguel-ayala');
